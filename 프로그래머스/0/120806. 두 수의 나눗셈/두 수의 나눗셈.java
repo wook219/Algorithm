1fdcaa365c73e5c821eaca80acfa817352528e40
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-16 23:25:05 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -9,3 +9,15 @@ class Solution {
 //         floor : Math객체의 소수점 버리는 메서드
     }
 }
+
+/*
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        return num1 * 1000 / num2;
+    }
+}
+
+이런 풀이도.. 있음.. 현타
+*/
