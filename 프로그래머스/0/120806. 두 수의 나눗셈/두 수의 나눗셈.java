38fca075fa87b5dbb51b5760b1727b085e27bbb6
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-16 23:22:38 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.22 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+       
+        double division = (double)num1 / num2 * 1000;
+        answer = (int)Math.floor(division);
+        
+        return answer;
+//         floor : Math객체의 소수점 버리는 메서드
+    }
+}
