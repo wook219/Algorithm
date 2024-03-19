0201862e1eb5e1cb3b0146db52de110207152aa0
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-19 20:46:31 +0900

    Update 양꼬치.java

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int n, int k) {
         // n은 양꼬치 인분수(12,000), k는 음료수 개수(2,000)
         int answer = 0;
-        int yang = 0;
         answer = n*12000+k*2000;
         for(int i = 1; i<=n; i++){
             if(i%10==0)
