971c46e5b21a05b10c83dd3bbaa53fb789335996
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-22 12:55:20 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.55 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,24 +1,18 @@
 class Solution {
     public String solution(int age) {
-        String reverse = "";
+        String answer = "";
         String [] engAge = {"a","b","c","d","e","f","g","h","i","j"};
 
         
         while(true){
             if(age < 10){
-                reverse += engAge[age];
+                answer = engAge[age] + answer;
                 break;
             }
-            reverse += engAge[age%10];
+            answer = engAge[age%10] + answer;
                 age /= 10;
         }
 
-        String answer = "";
-        
-        for(int i = reverse.length()-1; i>=0; i--){
-            answer += reverse.charAt(i);
-        }
-        
         return answer;
     }
 }
