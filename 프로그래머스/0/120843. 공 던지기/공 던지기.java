5f5536943d56bb875632c2b94b1e2a607df0fd1c
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-26 19:18:43 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
         int idx = k * 2 -2;
+        
         if(idx > numbers.length){
             idx = idx % (numbers.length);
         }
-        answer = numbers[idx];
-        return answer;
+
+        return numbers[idx];
     }
 }
