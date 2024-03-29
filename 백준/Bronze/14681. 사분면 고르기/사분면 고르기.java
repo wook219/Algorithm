07cef03cd3fb6ea50b9b0ee62e908119223954d3
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-29 14:35:15 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        System.out.println(x > 0 && y > 0 ? 1 : x < 0 && y > 0 ? 2 : x < 0 && y < 0 ? 3 : 4);
+    }
+}
