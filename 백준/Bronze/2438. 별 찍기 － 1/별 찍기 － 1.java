390ac5c4d78011cae8fa17e065332f29250d6c26
wook219 <141710721+wook219@users.noreply.github.com>
2024-03-31 17:10:49 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 308 ms, Memory: 18032 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
