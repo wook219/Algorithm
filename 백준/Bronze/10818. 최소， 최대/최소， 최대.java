7af931f41c0c6e30ee023231374d403c48531abc
wook219 <141710721+wook219@users.noreply.github.com>
2024-04-05 20:40:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 1748 ms, Memory: 318244 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < N; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.print(min + " ");
+        System.out.print(max);
+    }
+}
+
