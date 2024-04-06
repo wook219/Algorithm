69d0f6f1332e36a0ad8b9b182eb89590ece055c4
wook219 <141710721+wook219@users.noreply.github.com>
2024-04-06 21:56:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -4,15 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
+        char ch = scanner.nextLine().charAt(0);
 
-        if(str.charAt(0)>=97){
-            System.out.println('a' + (str.charAt(0) - 97));
-        }else if(str.charAt(0)>=65){
-            System.out.println('A' + (str.charAt(0) - 65));
-        }else if(str.charAt(0)>=48 &&str.charAt(0)<=57){
-            System.out.println('0' + (str.charAt(0) - 48));
-        }
+        System.out.println((int)ch);
     }
 }
 
