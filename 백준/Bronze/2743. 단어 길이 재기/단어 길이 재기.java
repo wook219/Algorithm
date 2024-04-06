b83902f83b970143aa806de66092196664ea9ff0
wook219 <141710721+wook219@users.noreply.github.com>
2024-04-06 21:18:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String S = scanner.nextLine();
+
+        System.out.println(S.length());
+    }
+}
+
