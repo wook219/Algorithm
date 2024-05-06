eeb9d0ef8201d4e026909c31c69b45726fd15a35
wook219 <141710721+wook219@users.noreply.github.com>
2024-05-06 13:37:28 +0900

    [Bronze V] Title: 연세대학교, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        System.out.println(N == 0 ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
