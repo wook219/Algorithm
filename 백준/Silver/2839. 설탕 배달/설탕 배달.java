9e972b2496baabeb8d7e1f95de86c0497b714a3d
wook219 <141710721+wook219@users.noreply.github.com>
2024-08-04 23:50:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 116 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 설탕 키로수
+        int count = 0; // 봉다리 수
+
+        for (int i = 0; i <= 1667; i++) {
+            for (int j = 0; j <= 1000; j++) {
+                int sum = (3 * i) + (5 * j);
+
+                if(sum == N){
+                    if(count == 0){
+                        count = i + j;
+                    }
+                    count = Math.min(count, i + j);
+                }
+            }
+        }
+
+        System.out.println(count != 0 ? count : -1);
+    }
+}
